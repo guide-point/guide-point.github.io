6e709bf5a1ff4319b66733aea3d1b18109f8d227
guide-point <53050583+guide-point@users.noreply.github.com>
2025-07-19 02:12:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const pages = [
     { name: "FRRO & Residence", url: "sections/frro.html" },
     { name: "Bank Account", url: "sections/bank.html" },
     { name: "SIM & WiFi", url: "sections/sim.html" },
-    { name: "Universities in Pune", url: "sections/universities.html" },
+    { name: "Universities in Pune", url: "sections/edu.html" },
     { name: "Aadhaar & PAN", url: "sections/important-cards.html" },
-    { name: "Contact", url: "sections/contact.html" }
+    { name: "blog", url: "sections/blog.html" },
+    { name: "c-form", url: "sections/c-form-guide.html" },
+    { name: "rp, url: "sections/step-by-step-guide.html" }
 ];
 
 function searchPages() {
